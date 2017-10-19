46c40081290ac1ee106bbce13d798aff4b58195f
Jorge Vergara <javebratt@gmail.com>
2017-10-19 18:55:02 -0500

    Changes default tab pages for new lazy loaded pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -18,7 +15,7 @@ import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { firebaseConfig } from './credentials';
 
 @NgModule({
-  declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+  declarations: [MyApp, TabsPage],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
@@ -27,7 +24,7 @@ import { firebaseConfig } from './credentials';
     AngularFirestoreModule.enablePersistence()
   ],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
+  entryComponents: [MyApp, TabsPage],
   providers: [
     StatusBar,
     SplashScreen,
