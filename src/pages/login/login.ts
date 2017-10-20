c7d49e2b82b132c87e71af0847cfd75e583b1bd6
Jorge Vergara <javebratt@gmail.com>
2017-10-19 19:15:29 -0500

    Creates the login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,73 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {
+  Alert,
+  AlertController,
+  IonicPage,
+  Loading,
+  LoadingController,
+  NavController
+} from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator } from '../../validators/email';
+import { AuthProvider } from '../../providers/auth/auth';
+import { TabsPage } from '../tabs/tabs';
 
 @IonicPage()
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm: FormGroup;
+  constructor(
+    public navCtrl: NavController,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public authProvider: AuthProvider,
+    formBuilder: FormBuilder
+  ) {
+    this.loginForm = formBuilder.group({
+      email: [
+        '',
+        Validators.compose([Validators.required, EmailValidator.isValid])
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(6)])
+      ]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  async loginUser(): Promise<any> {
+    if (!this.loginForm.valid) {
+      console.log('Form not ready');
+    } else {
+      let loading: Loading;
+      loading = this.loadingCtrl.create();
+      loading.present();
+
+      const email: string = this.loginForm.value.email;
+      const password: string = this.loginForm.value.password;
+      try {
+        await this.authProvider.loginUser(email, password);
+        await loading.dismiss();
+        this.navCtrl.setRoot(TabsPage);
+      } catch (error) {
+        await loading.dismiss();
+        const alert: Alert = this.alertCtrl.create({
+          message: error.message,
+          buttons: [{ text: 'Ok', role: 'cancel' }]
+        });
+        alert.present();
+      }
+    }
   }
 
+  goToSignup(): void {
+    this.navCtrl.push('SignupPage');
+  }
+
+  goToResetPassword(): void {
+    this.navCtrl.push('PasswordResetPage');
+  }
 }
