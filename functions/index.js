78c1635732595b137480df2e1ad9bbf8d26e56f8
Jorge Vergara <javebratt@gmail.com>
2017-10-27 17:12:15 -0500

    Creates Cloud Function to add a new user to the team

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.createTeamMember = functions.firestore
+  .document(`teamProfile/{teamId}/teamMemberList/{newUserId}`)
+  .onCreate(event => {
+    const id = event.data.data().id;
+    const email = event.data.data().email;
+    const teamId = event.data.data().teamId;
+
+    return admin
+      .auth()
+      .createUser({
+        uid: id,
+        email: email,
+        password: '123456789'
+      })
+      .then(newUser => {
+        admin
+          .firestore()
+          .doc(`userProfile/${newUser.uid}/`)
+          .set({
+            email: email,
+            id: id,
+            teamAdmin: false,
+            teamId: teamId
+          });
+      })
+      .catch(error => console.error('Error creating the user', error));
+  });
